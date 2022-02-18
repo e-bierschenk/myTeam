dc49204772b0499bf18f886330795065d8a72945
eo-lind <eo.lind@gmail.com>
2022-02-18 10:32:53 -0600

    added birthday function

---
+++ main.js
@@ -0,0 +1,10 @@
+const birthdayLog = () => {
+    let ericBirthday = "6-12-68"
+    let coryBirthday = "2-18-42"
+    let oliviaBirthday = "12-26-81"
+    console.log(ericBirthday)
+    console.log(coryBirthday)
+    console.log(oliviaBirthday)
+}
+
+const listOfBirthdays = birthdayLog()
