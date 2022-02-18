2d39cae1722d4ad60949050b966f233a5c09a7a1
k-sarito <coryclark95@gmail.com>
2022-02-18 10:48:26 -0600

    added a function that logs all team names with a message

---
+++ main.js
@@ -0,0 +1,13 @@
+const name1 = "Cory Clark"
+const name2 = "Olivia Lind" 
+const name3 = "Eric Bierschenk"
+
+
+
+const nameLog = (obj) => {
+    console.log(`My name is ${obj} and I approve this website`)
+}
+
+nameLog(name1)
+nameLog(name2)
+nameLog(name3)
